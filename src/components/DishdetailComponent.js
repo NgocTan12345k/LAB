312dc7f8f8b1881eee004e3aa7ade25fe26bb9de
NgocTan12345k <kiemtienonlinee1@gmail.com>
2022-06-06 23:10:27 +0700

    LAB04_1 hoan thien

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -19,14 +19,14 @@ class DishDetail extends Component {
     RenderComments(comments) {
         if (comments != null) {
             return (
-                <div className="col-12 cold-md-5 m-1">
+                <div className="col-12 col-md-5 m-1">
                     <h4>Comments</h4>
                     <ul className="list-unstyled">
                         {comments.map((comment) => {
                             return (
                                 <li key={comments.id}>
                                     <p>{comment.comment}</p>
-                                    <p>-- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.updatedAt)))}</p>
+                                    <p>-- {comment.author}, {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit' }).format(new Date(Date.parse(comment.date)))}</p>
                                 </li>
                             );
                         })}
