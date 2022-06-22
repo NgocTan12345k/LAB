fdff52757713d94b7622a23a7f56e0efcfecdbe1
NgocTan12345k <kiemtienonlinee1@gmail.com>
2022-06-22 23:10:08 +0700

    Intro. to Redux

--- src/App.js
+++ src/App.js
@@ -5,17 +5,23 @@ import { Navbar, NavbarBrand, navbarBrand } from 'reactstrap';
 import './App.css';
 import { BrowserRouter } from 'react-router-dom';
 import { DISHES } from './shared/dishes';
+import { Provider } from "react-redux";
+import { ConfigureStore } from "./redux/configureStore";
+
+const store = ConfigureStore();
 
 class App extends Component {
 
 
   render() {
     return (
-      <BrowserRouter>
+      <Provider store={store}>
+        <BrowserRouter>
       <div className="App">
         <Main />
       </div>
     </BrowserRouter>
+      </Provider>
     );
   }
 }
