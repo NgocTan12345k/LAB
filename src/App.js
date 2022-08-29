8f41575b18cbde1f20b7b03015808c138d721375
NgocTan12345k <kiemtienonlinee1@gmail.com>
2022-08-29 23:08:08 +0700

    Components Part 1

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import Menu from "./Components/MenuComponent";
+import "./App.css";
+import { Navbar, NavbarBrand } from "reactstrap";
+import { Component } from "react";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Navbar dark color="primary">
+          <div className="container">
+            <NavbarBrand href="#">Ristorante Con Fusion</NavbarBrand>
+          </div>
+        </Navbar>
+        <Menu />
+      </div>
+    );
+  }
+}
+
+export default App;
